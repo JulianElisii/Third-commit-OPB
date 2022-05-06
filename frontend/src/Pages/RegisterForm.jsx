fadf1de6a38989477d02a249b165ab21c124f3cc
Julian <julianelisii998@gmail.com>
2022-05-06 19:29:46 +0200

    second commit

---
+++ frontend/src/Pages/RegisterForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SingUp from "../components/SingUp"
+
+const RegisterForm = () => {
+    return (
+        <div>
+            <SingUp/>
+        </div>
+    );
+}
+
+export default RegisterForm;
